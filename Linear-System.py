a4e8dd1a7fc5a195085297b485e5cd82d51acf96
tristanmeyer <42872117+tristanmeyer@users.noreply.github.com>
2019-01-23 20:08:31 -0500

    Updated from Brython Server: 1/23/2019 8:08:31 PM

--- Linear-System.py
+++ Linear-System.py
@@ -1,1 +1,15 @@
+#Tristan Meyer
+#Linear System of Equations
 
+A = float(input("Enter A: "))
+B = float(input("Enter B: "))
+C = float(input("Enter C: "))
+D = float(input("Enter D: "))
+E = float(input("Enter E: "))
+F = float(input("Enter F: "))
+
+y = ((F*A)-(D*C))/((-D*B)+(E*A))
+x = (C-(B*(y)))/A
+sy = str(y)
+sx = str(x)
+print("The solution is ("+sx+","+sy+")")
